d761e92c707a007e068008a0c85446fbef7c4880
roubkar <r.karapetyan@createch.am>
2019-06-13 18:04:44 +0400

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,154 @@
+import * as React from "react";
+import { Animated } from "r-animated";
+
+interface ICarouselProps
+{
+  slides: React.ReactElement[];
+  duration: number;
+  animationDuration: number;
+  animationTimingFunction: string;
+  animationType: "fade" | "slideLeft" | "slideRight";
+  animationDelay: number;
+  customAnimation: {
+    active: {
+      from: React.CSSProperties,
+      to: React.CSSProperties
+    },
+    next: {
+      from: React.CSSProperties,
+      to: React.CSSProperties}
+  }
+}
+
+interface ICarouselState
+{
+  active: number;
+  nextActive: number;
+}
+
+export default function Carousel(props: ICarouselProps)
+{
+  let [state, dispatch] = React.useReducer(
+    (state: ICarouselState, action: { type: string }) =>
+    {
+      switch (action.type)
+      {
+        case "next":
+          return {
+            active: state.nextActive,
+            nextActive: (state.nextActive + 1) % props.slides.length
+          };
+        case "bringNext":
+          return {
+            active: state.active,
+            nextActive: state.nextActive
+          };
+        default:
+          throw new Error();
+      }
+    },
+    { active: -1, nextActive: 0 }
+  );
+  React.useEffect(() =>
+  {
+    let timerId = setTimeout(() =>
+    {
+      clearTimeout(timerId);
+      dispatch({ type: "next" });
+    }, props.duration || 1000);
+    return () =>
+    {
+      clearTimeout(timerId);
+    };
+  }, [state.nextActive]);
+  React.useEffect(() =>
+  {
+    let timerId = setTimeout(() =>
+    {
+      clearTimeout(timerId);
+      dispatch({ type: "bringNext" });
+    }, props.duration || 1000);
+    return () =>
+    {
+      clearTimeout(timerId);
+    };
+  }, [state.active]);
+  return (
+    <div style={styles.container} className="animated-carousel-container">
+      <Animated
+        duration={props.animationDuration}
+        timingFunction={props.animationTimingFunction}
+        {...animationTypes[props.animationType] &&
+        animationTypes[props.animationType].active}
+        {...props.customAnimation && props.customAnimation.active}
+        delay={props.animationDelay}
+        key={`active${state.nextActive}`}
+      >
+        {style => (
+          <div style={{ ...styles, ...style }} className="animated-carousel-item">
+            {props.slides[state.active]}
+          </div>
+        )}
+      </Animated>
+      <Animated
+        duration={props.animationDuration}
+        timingFunction={props.animationTimingFunction}
+        {...animationTypes[props.animationType] &&
+        animationTypes[props.animationType].next}
+        {...props.customAnimation && props.customAnimation.next}
+        delay={props.animationDelay}
+        key={`${state.nextActive}`}
+      >
+        {(style: React.CSSProperties) => (
+          <div style={{ ...styles, ...style }} className="animated-carousel-item">
+            {props.slides[state.nextActive]}
+          </div>
+        )}
+      </Animated>
+    </div>
+  );
+}
+
+let styles = {
+  container: {
+    position: "relative",
+    overflow: "hidden"
+  },
+  item: {
+    position: "absolute",
+    width: "100%"
+  }
+};
+
+const animationTypes = {
+  fade: {
+    active: {
+      from: { opacity: 1 },
+      to: { opacity: 0 }
+    },
+    next: {
+      from: { opacity: 0.19 },
+      to: { opacity: 1 }
+    }
+  },
+  slideLeft: {
+    active: {
+      from: { transform: "translateX(0)" },
+      to: { transform: "translateX(-100%)" }
+    },
+    next: {
+      from: { transform: "translateX(100%)" },
+      to: { transform: "translateX(0%)" }
+    }
+  },
+  slideRight: {
+    active: {
+      from: { transform: "translateX(0)" },
+      to: { transform: "translateX(100%)" }
+    },
+    next: {
+      from: { transform: "translateX(-100%)" },
+      to: { transform: "translateX(0%)" }
+    }
+  }
+};
