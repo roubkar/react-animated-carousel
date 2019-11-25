af375676cd5739bc95cb1eee55bccfdceb89508a
roubkar <karapetyanrouben@gmail.com>
2019-11-25 10:46:56 +0400

    bug fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
-import { useAnimation } from "r-animated";
+import { Animated } from "r-animated";
 
-interface ICarouselProps
-{
+interface ICarouselProps {
   slides: React.ReactElement[];
   duration?: number;
   animationDuration?: number;
@@ -12,113 +11,115 @@ interface ICarouselProps
   withNavigation?: boolean;
   customAnimation?: {
     active: {
-      from: React.CSSProperties,
-      to: React.CSSProperties
-    },
+      from: React.CSSProperties;
+      to: React.CSSProperties;
+    };
     next: {
-      from: React.CSSProperties,
-      to: React.CSSProperties
-    }
-  }
+      from: React.CSSProperties;
+      to: React.CSSProperties;
+    };
+  };
 }
 
-interface ICarouselState
-{
+interface ICarouselState {
   active: number;
   nextActive: number;
-  reset: boolean;
 }
 
-export default function Carousel(props: ICarouselProps): React.ReactElement
-{
+export default function Carousel(props: ICarouselProps): React.ReactElement {
   let [state, dispatch] = React.useReducer(
-    (state: ICarouselState, action: { type: string, index?: number }) =>
-    {
-      switch (action.type)
-      {
+    (state: ICarouselState, action: { type: string; index?: number }) => {
+      switch (action.type) {
         case "NEXT":
           return {
             active: state.nextActive,
-            nextActive: (state.nextActive + 1) % props.slides.length,
-            reset: !state.reset
+            nextActive: (state.nextActive + 1) % props.slides.length
           };
         case "BRING_NEXT":
           return {
             active: state.active,
-            nextActive: state.nextActive,
-            reset: !state.reset
+            nextActive: state.nextActive
           };
         case "CUSTOM":
           return {
-            active: state.active,
-            nextActive: action.index,
-            reset: !state.reset
+            active: state.nextActive,
+            nextActive: action.index
           };
         default:
           throw new Error();
       }
     },
-    { active: -1, nextActive: 0, reset: false }
+    { active: -1, nextActive: 0 }
   );
 
-  let activeStyles: React.CSSProperties = useAnimation({
-    duration: props.animationDuration,
-    timingFunction: props.animationTimingFunction,
-    ...animationTypes[props.animationType] && animationTypes[props.animationType].active,
-    ...props.customAnimation && props.customAnimation.active,
-    delay: props.animationDelay || 100,
-    reset: state.reset
-  });
+  let timerId1: React.MutableRefObject<number> = React.useRef();
+  let timerId2: React.MutableRefObject<number> = React.useRef();
 
-  let nextStyles: React.CSSProperties = useAnimation({
-    duration: props.animationDuration,
-    timingFunction: props.animationTimingFunction,
-    ...animationTypes[props.animationType] && animationTypes[props.animationType].next,
-    ...props.customAnimation && props.customAnimation.next,
-    delay: props.animationDelay || 100,
-    reset: state.reset
-  });
-
-  let timerId1 = React.useRef();
-  let timerId2 = React.useRef();
-
-  React.useEffect(() =>
-  {
-    timerId1.current = setTimeout(() =>
-    {
+  React.useEffect(() => {
+    clearTimeout(timerId1.current);
+    timerId1.current = setTimeout(() => {
       dispatch({ type: "NEXT" });
     }, props.duration || 1000);
     return () => clearTimeout(timerId1.current);
-  }, [state.nextActive]);
+  }, [state.nextActive, props.duration]);
 
-  React.useEffect(() =>
-  {
-    timerId2.current = setTimeout(() =>
-    {
+  React.useEffect(() => {
+    clearTimeout(timerId2.current);
+    timerId2.current = setTimeout(() => {
       dispatch({ type: "BRING_NEXT" });
     }, props.duration || 1000);
     return () => clearTimeout(timerId2.current);
-  }, [state.active]);
+  }, [state.active, props.duration]);
 
-  function setIndex(index: number): void
-  {
+  function setIndex(index: number): void {
     clearTimeout(timerId1.current);
     clearTimeout(timerId2.current);
     dispatch({ type: "CUSTOM", index });
-    timerId1.current = setTimeout(() =>
-    {
-      dispatch({ type: "NEXT" });
+    timerId1.current = setTimeout(() => {
+      dispatch({ type: "BRING_NEXT" });
     }, props.duration || 1000);
   }
 
   return (
-    <div style={styles.container} className="animated-carousel-container">
-      <div style={{ ...styles.item, ...activeStyles }} className="animated-carousel-item">
-        {props.slides[state.active]}
-      </div>
-      <div style={{ ...styles.item, ...nextStyles }} className="animated-carousel-item">
-        {props.slides[state.nextActive]}
-      </div>
+    <div
+      style={styles.container}
+      className="animated-carousel-container"
+      key={state.nextActive + 10}
+    >
+      <Animated
+        duration={props.animationDuration}
+        timingFunction={props.animationTimingFunction}
+        {...animationTypes[props.animationType] &&
+          animationTypes[props.animationType].active}
+        {...props.customAnimation && props.customAnimation.active}
+        delay={props.animationDelay || 100}
+      >
+        {style => (
+          <div
+            style={{ ...styles.item, ...style }}
+            className="animated-carousel-item"
+          >
+            {props.slides[state.active]}
+          </div>
+        )}
+      </Animated>
+      <Animated
+        duration={props.animationDuration}
+        timingFunction={props.animationTimingFunction}
+        {...animationTypes[props.animationType] &&
+          animationTypes[props.animationType].next}
+        {...props.customAnimation && props.customAnimation.next}
+        delay={props.animationDelay || 100}
+      >
+        {style => (
+          <div
+            style={{ ...styles.item, ...style }}
+            className="animated-carousel-item"
+          >
+            {props.slides[state.nextActive]}
+          </div>
+        )}
+      </Animated>
       {props.withNavigation ? (
         <div className="animated-carousel-dots">
           {props.slides.map((slide, index) => (
@@ -126,7 +127,7 @@ export default function Carousel(props: ICarouselProps): React.ReactElement
               key={`${index}`}
               className={`animated-carousel-dot ${
                 index === state.nextActive ? "active" : ""
-                }`}
+              }`}
               onClick={(e: React.SyntheticEvent) => setIndex(index)}
             />
           ))}
@@ -136,7 +137,7 @@ export default function Carousel(props: ICarouselProps): React.ReactElement
   );
 }
 
-let styles = {
+let styles: { [key: string]: React.CSSProperties } = {
   container: {
     position: "relative",
     overflow: "hidden",
